41566d84152531faf6065120061ff54b3c673916
zhangyi <qs252201@gmail.com>
2026-06-18 09:29:52 +0800

    Initial Commit

---
+++ AptvMerge/AptvMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AptvMergeApp.swift
+//  AptvMerge
+//
+//  Created by LPP on 2026/6/18.
+//
+
+import SwiftUI
+
+@main
+struct AptvMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
