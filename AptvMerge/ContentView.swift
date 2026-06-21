31385ed3fbd6942eb2480b6751f5b26f4eb53539
zhangyi <qs252201@gmail.com>
2026-06-21 10:22:24 +0800

    Bump version to 2.3

--- AptvMerge/ContentView.swift
+++ AptvMerge/ContentView.swift
@@ -328,7 +328,7 @@ struct ContentView: View {
     }
 
     private var appVersion: String {
-        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "2.2"
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "2.3"
     }
 
     private var phaseText: String {
